e2bd2ec966656256f35417c4c9b5a8c7aced7c79
sparsh-mehta-1709 <sparsh.mehta@scaler.com>
2024-08-28 02:26:21 +0530

    Update main.py

--- main.py
+++ main.py
@@ -148,10 +148,11 @@ def generate_sql_query(user_input):
     LEFT JOIN scaler_ebdb_interviewbit_test_session_problems ibtsp ON ibtsp.test_session_id = ibts.id
     28. WHENEVER ASSIGNMENT PSP OR HW PSP IS ASKED CALCULATE THE total_assignment_problems_solved AND total_assignment_problems FIRSTLY AND AFTER THAT GIVE THE PERCENTAGE
     29. In where clause do not use any subquery
-    30. To know about how many paid learners come in a month use punched_leads -> applicant_tracks -> super_batches -> super_batch_groups -> cohorts and alweays use cohort name as filter
+    30. To know about how many paid learners come in a month use 
+    punched_leads -> applicant_tracks (using applicant_track_id) -> super_batches (using super_batch_id) -> super_batch_groups (using super_batch_group_id) -> cohorts (using cohort_id) and alweays use cohort name as filter
     31. To know about Net Promoter Score (NPS) and to get nps formula is (promter-detractor)/(promoter+neutral+detractor)*100 
     where promoter is 9 or 10, neutral is 7 or 8 and detractor is 1 to 6 use this table scaler_ebdb_interviewbit_form_responses
-    use 
+    use this query to get the data
     select distinct m.id as responder_id,scaler_ebdb_users.email as email,scaler_ebdb_interviewbit_form_responses.response::int as nps_rating,scaler_ebdb_interviewbit_form_responses.created_at::date as nps_date
 FROM scaler_ebdb_interviewbit_form_responses
 LEFT JOIN scaler_ebdb_mentee_batches ON scaler_ebdb_mentee_batches.mentee_id = scaler_ebdb_interviewbit_form_responses.responder_id
